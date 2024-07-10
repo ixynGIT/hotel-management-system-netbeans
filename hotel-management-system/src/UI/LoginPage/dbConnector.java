f1d97a460613b477b9d87ce33e54eb63e7e5f712
ixynGIT <ixynrs@gmail.com>
2024-07-11 00:31:49 +0800

    only backed up the unfinished version. The finished got corrupted

---
+++ hotel-management-system/src/UI/LoginPage/dbConnector.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UI.LoginPage;
+
+import java.sql.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author hp
+ */
+public class dbConnector {
+    private Connection connect;
+    
+    public dbConnector(){
+            try{
+                connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/plasabas_db", "root", "");
+            }catch(SQLException ex) {
+                System.out.println("Can't connect to database:"+ex.getMessage());
+            }
+    }
+    
+    public ResultSet getData(String sql)throws SQLException{
+        Statement stmt = connect.createStatement();
+        ResultSet rslt = stmt.executeQuery(sql);
+        return rslt;
+    }
+    
+    public int insertData(String sql){
+        int result;
+        try{
+            PreparedStatement pst = connect.prepareStatement(sql);
+            pst.executeUpdate();
+            System.out.println("Data inserted successfully");
+            pst.close();
+            result=1;
+        }catch(SQLException ex){
+            System.out.println("Connection Error: "+ex);
+            result=0;
+        }
+        return result;
+    }
+    
+    public void updateData(String sql){
+        try{
+            PreparedStatement pst = connect.prepareStatement(sql);
+            int rowsUpdated = pst.executeUpdate();
+            if (rowsUpdated > 0){
+                JOptionPane.showMessageDialog(null, "Data Updated Successfully!");
+            }else{
+                JOptionPane.showMessageDialog(null, "Data Failed to Update");
+                }
+            pst.close();
+        }catch(SQLException ex){
+            System.out.println("Connection Error: "+ex);   
+            }  
+    }
+    
+}
---
+++ hotel-management-system/src/UI/Pages/RoundedPanel.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UI.Pages;
+
+import java.awt.*;
+import javax.swing.JPanel;
+
+
+public class RoundedPanel extends JPanel
+    {
+        private Color backgroundColor;
+        private int cornerRadius = 15;
+
+        public RoundedPanel(LayoutManager layout, int radius) {
+            super(layout);
+            cornerRadius = radius;
+        }
+
+        public RoundedPanel(LayoutManager layout, int radius, Color bgColor) {
+            super(layout);
+            cornerRadius = radius;
+            backgroundColor = bgColor;
+        }
+
+        public RoundedPanel(int radius) {
+            super();
+            cornerRadius = radius;
+        }
+
+        public RoundedPanel(int radius, Color bgColor) {
+            super();
+            cornerRadius = radius;
+            backgroundColor = bgColor;
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Dimension arcs = new Dimension(cornerRadius, cornerRadius);
+            int width = getWidth();
+            int height = getHeight();
+            Graphics2D graphics = (Graphics2D) g;
+            graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+            
+            if (backgroundColor != null) {
+                graphics.setColor(backgroundColor);
+            } else {
+                graphics.setColor(getBackground());
+            }
+            graphics.fillRoundRect(0, 0, width-1, height-1, arcs.width, arcs.height);
+            graphics.setColor(getForeground());
+            
+        }
+ }
